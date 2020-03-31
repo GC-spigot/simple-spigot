238d6a998afb8fa2aebc7143323dcebfe1a08420
Hyfe-JavaDebug <hyfe@yahoo.com>
2020-03-31 11:02:33 +0200

    time formatting fix

--- src/main/java/me/hyfe/simplespigot/service/General.java
+++ src/main/java/me/hyfe/simplespigot/service/General.java
@@ -10,23 +10,23 @@ public class General {
         long hours = initialSeconds % 86400 / 3600;
         long minutes = initialSeconds % 3600 / 60;
         long seconds = initialSeconds % 60;
-        if (years < 1) {
-            return String.format("%dmo %dw %dd %dh %dm %ds", months, weeks, days, hours, minutes, seconds);
-        }
-        if (months < 1) {
-            return String.format("%dw %dd %dh %dm %ds", weeks, days, hours, minutes, seconds);
-        }
-        if (weeks < 1) {
-            return String.format("%dd %dh %dm %ds", days, hours, minutes, seconds);
-        }
-        if (days < 1) {
-            return String.format("%dh %dm %ds", hours, minutes, seconds);
+        if (minutes < 1) {
+            return String.format("%ds", seconds);
         }
         if (hours < 1) {
             return String.format("%dm %ds", minutes, seconds);
         }
-        if (minutes < 1) {
-            return String.format("%ds", seconds);
+        if (days < 1) {
+            return String.format("%dh %dm %ds", hours, minutes, seconds);
+        }
+        if (weeks < 1) {
+            return String.format("%dd %dh %dm %ds", days, hours, minutes, seconds);
+        }
+        if (months < 1) {
+            return String.format("%dw %dd %dh %dm %ds", weeks, days, hours, minutes, seconds);
+        }
+        if (years < 1) {
+            return String.format("%dmo %dw %dd %dh %dm %ds", months, weeks, days, hours, minutes, seconds);
         }
         return String.format("%dy %dmo %dw %dd %dh %dm %ds", years, months, weeks, days, hours, minutes, seconds);
     }
