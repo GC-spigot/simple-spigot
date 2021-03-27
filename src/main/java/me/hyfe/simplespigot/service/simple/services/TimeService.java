53377c9c49f58472fcc83a830831cba4f3728691
Zak <shearmanzak@gmail.com>
2021-03-27 23:50:02 +0000

    Add time formatting for TimeService

--- src/main/java/me/hyfe/simplespigot/service/simple/services/TimeService.java
+++ src/main/java/me/hyfe/simplespigot/service/simple/services/TimeService.java
@@ -3,8 +3,19 @@ package me.hyfe.simplespigot.service.simple.services;
 import java.util.concurrent.TimeUnit;
 
 public class TimeService {
+    private final SimpleTimeFormat defaultFormat = new SimpleTimeFormat("%dy %dmo %dw %dd %dh %dm %ds",
+            "%dmo %dw %dd %dh %dm %ds",
+            "%dw %dd %dh %dm %ds",
+            "%dd %dh %dm %ds",
+            "%dh %dm %ds",
+            "%dm %ds",
+            "%ds");
 
     public String format(TimeUnit timeUnit, long duration) {
+        return this.format(this.defaultFormat, timeUnit, duration);
+    }
+
+    public String format(SimpleTimeFormat timeFormat, TimeUnit timeUnit, long duration) {
         long origin = timeUnit.toSeconds(duration);
         long years = origin / 31536000;
         long months = origin % 31536000 / 2592000; // Months calculated with 30 days.
@@ -15,23 +26,71 @@ public class TimeService {
         long seconds = origin % 60;
 
         if (years >= 1) {
-            return String.format("%dy %dmo %dw %dd %dh %dm %ds", years, months, weeks, days, hours, minutes, seconds);
+            return String.format(timeFormat.getYearsMonths(), years, months, weeks, days, hours, minutes, seconds);
         }
         if (months >= 1) {
-            return String.format("%dmo %dw %dd %dh %dm %ds", months, weeks, days, hours, minutes, seconds);
+            return String.format(timeFormat.getMonthsWeeks(), months, weeks, days, hours, minutes, seconds);
         }
         if (weeks >= 1) {
-            return String.format("%dw %dd %dh %dm %ds", weeks, days, hours, minutes, seconds);
+            return String.format(timeFormat.getWeeksDays(), weeks, days, hours, minutes, seconds);
         }
         if (days >= 1) {
-            return String.format("%dd %dh %dm %ds", days, hours, minutes, seconds);
+            return String.format(timeFormat.getDaysHours(), days, hours, minutes, seconds);
         }
         if (hours >= 1) {
-            return String.format("%dh %dm %ds", hours, minutes, seconds);
+            return String.format(timeFormat.getHoursMinutes(), hours, minutes, seconds);
         }
         if (minutes >= 1) {
-            return String.format("%dm %ds", minutes, seconds);
+            return String.format(timeFormat.getMinutesSeconds(), minutes, seconds);
         }
-        return String.format("%ds", seconds);
+        return String.format(timeFormat.getSeconds(), seconds);
+    }
+
+    public static class SimpleTimeFormat {
+        private final String yearsMonths;
+        private final String monthsWeeks;
+        private final String weeksDays;
+        private final String daysHours;
+        private final String hoursMinutes;
+        private final String minutesSeconds;
+        private final String seconds;
+
+        public SimpleTimeFormat(String yearsMonths, String monthsWeeks, String weeksDays, String daysHours, String hoursMinutes, String minutesSeconds, String seconds) {
+            this.yearsMonths = yearsMonths;
+            this.monthsWeeks = monthsWeeks;
+            this.weeksDays = weeksDays;
+            this.daysHours = daysHours;
+            this.hoursMinutes = hoursMinutes;
+            this.minutesSeconds = minutesSeconds;
+            this.seconds = seconds;
+        }
+
+        public String getYearsMonths() {
+            return this.yearsMonths;
+        }
+
+        public String getMonthsWeeks() {
+            return this.monthsWeeks;
+        }
+
+        public String getWeeksDays() {
+            return this.weeksDays;
+        }
+
+        public String getDaysHours() {
+            return this.daysHours;
+        }
+
+        public String getHoursMinutes() {
+            return this.hoursMinutes;
+        }
+
+        public String getMinutesSeconds() {
+            return this.minutesSeconds;
+        }
+
+        public String getSeconds() {
+            return this.seconds;
+        }
     }
 }
